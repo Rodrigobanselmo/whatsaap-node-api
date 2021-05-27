2413d18190a147fa82d4045215b25797755d6a37
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-05-27 18:51:35 -0300

    ok

--- api.js
+++ api.js
@@ -1,91 +1,119 @@
-const express = require("express");
-const fs = require('fs');
-const axios = require('axios');
-const shelljs = require('shelljs');
-
-const config = require('./config.json');
-const { Client } = require('whatsapp-web.js');
-const SESSION_FILE_PATH = './session.json';
-
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
-
-process.title = "whatsapp-node-api";
-global.client = new Client({
-    puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--unhandled-rejections=strict'
-    ]},
-    session: sessionCfg
-});
-
-global.authed = false;
-
-const app = express();
-
-const port = process.env.PORT || config.port;
-//Set Request Size Limit 50 MB
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-client.on('qr', qr => {
-    fs.writeFileSync('./components/last.qr', qr);
-});
-
-
-client.on('authenticated', (session) => {
-    console.log("AUTH!");
-    sessionCfg = session;
-
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-        authed = true;
-    });
-
-    try {
-        fs.unlinkSync('./components/last.qr')
-    } catch(err) {}
-});
-
-client.on('auth_failure', () => {
-    console.log("AUTH Failed !")
-    sessionCfg = ""
-    process.exit()
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', msg => {
-    if (config.webhook.enabled) {
-        axios.post(config.webhook.path, { msg })
-    }
-})
-client.initialize();
-
-const chatRoute = require('./components/chatting');
-const groupRoute = require('./components/group');
-const authRoute = require('./components/auth');
-const contactRoute = require('./components/contact');
-
-app.use(function(req, res, next){
-    console.log(req.method + ' : ' + req.path);
-    next();
-});
-app.use('/chat',chatRoute);
-app.use('/group',groupRoute);
-app.use('/auth',authRoute);
-app.use('/contact',contactRoute);
-
-app.listen(port, () => {
-    console.log("Server Running Live on Port : " + port);
-});
+/**
+ * ⚡⚡⚡ DECLARAMOS LAS LIBRERIAS y CONSTANTES A USAR! ⚡⚡⚡
+ */
+ const express = require('express');
+ const { Client, MessageMedia } = require('whatsapp-web.js');
+ const app = express();
+ app.use(express.urlencoded({ extended: true }))
+ let client;
+ let sessionData;
+ 
+ const PORT = process.env.PORT || 9000;
+ 
+ const sendMessage = (number = null, text = null) => {
+     number = number.replace('@c.us', '');
+     number = `${number}@c.us`
+     const message = text || `Mensagem enviada`;
+     client.sendMessage(number, message);
+     console.log(`Enviando mensajes....`);
+ }
+ 
+ const listenMessage = () => {
+     client.on('message', async msg => {
+         const { from, to, body } = msg;
+         await greetCustomer(from);
+         
+         console.log('body',body);
+         
+         await replyAsk(from, body);
+ 
+     });
+ }
+ 
+ const replyAsk = (from, answer) => new Promise((resolve, reject) => {
+     console.log(`---------->`, answer);
+     if (answer === 'yes') {
+         sendMessage(from, 'OK')
+         resolve(true)
+     }
+ 
+ })
+ 
+ const withSession = (sessionJson) => {
+ 
+     sessionData = sessionJson;
+     client = new Client({
+         session: sessionData
+     });
+ 
+     client.on('ready', () => {
+         console.log('Client is ready!');
+         connectionReady();
+     });
+ 
+     client.on('auth_failure', () => {
+         console.log('** Error de autentificacion vuelve a generar el QRCODE (Borrar el archivo session.json) **');
+     })
+ 
+     client.initialize();
+ }
+ 
+ const withOutSession = () => {
+     console.log('No tenemos session guardada');
+     client = new Client();
+     // client.on('qr', qr => {
+     //     qrcode.generate(qr, { small: true });
+     // });
+ 
+     client.on('ready', () => {
+         console.log('Client is ready!');
+         connectionReady();
+     });
+ 
+     client.on('auth_failure', () => {
+         console.log('** Error de autentificacion vuelve a generar el QRCODE **');
+     })
+ 
+ 
+     client.on('authenticated', (session) => {
+         // Guardamos credenciales de de session para usar luego
+         sessionData = session;
+         // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+         //     if (err) {
+         //         console.log(err);
+         //     }
+         // });
+     });
+ 
+     client.initialize();
+ }
+ 
+ const connectionReady = () => {
+     listenMessage();
+ }
+ 
+ const greetCustomer = (from) => new Promise((resolve, reject) => {
+     from = from.replace('@c.us', '');
+     if (from === '555180173688') {
+         sendMessage(from, 'OK')
+         resolve(true)
+     }
+    //  sendMessage(from, 'firstMessage')
+    //  resolve(true)
+ })
+ 
+ const sendMessagePost = (req, res) => {
+     const { message, number } = req.body
+     console.log(message, number);
+     sendMessage(number, message)
+     res.send({ status: 'Enviado!' })
+ }
+ 
+ app.post('/send', sendMessagePost);
+ 
+ 
+ (true) ? withSession({"WABrowserId":"\"YzsEz8STFWxHHGrUWYVPJw==\"","WASecretBundle":"{\"key\":\"VyVIBUL1qqVw1QOR+sPZ96FM8ybTpeAaKxou/NlQEgk=\",\"encKey\":\"gjFHuBGiN/eMbE+66zrM+imKE/PkPzyJldYd/4SdLbk=\",\"macKey\":\"VyVIBUL1qqVw1QOR+sPZ96FM8ybTpeAaKxou/NlQEgk=\"}","WAToken1":"\"y5NKQd884upkeghOECf1jS6/1kIzwSfKW6LkcJeGWHA=\"","WAToken2":"\"1@UMfMBVfPfpTSWFx84ro1POW2iPG0Ccjdj3WCt4knU691l9MbBd6f+cQrx9GSCLIS6QLrz4qb8OLvjQ==\""}) : withOutSession();
+ 
+ app.listen(PORT, () => {
+     console.log('Server ready!');
+ })
